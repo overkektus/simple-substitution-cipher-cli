361952e92b04532e5bda7dba180e139d3105604d
Egor Piskunou <egor.piskunou@gmail.com>
2018-02-22 11:05:46 +0300

    done

--- cli.js
+++ cli.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
 const argv = require('yargs')
   .version()
   .usage('Usage: nodejs-cli-app <command> [options]')
-  .demandCommand(1, 'You need at least one command before moving on')
+  .demandCommand(1, chalk.blue('You need at least one command before moving on'))
+  .command(require('./lib/encr'))
+  .command(require('./lib/decr'))
+  .command(require('./lib/genkey'))
   .help('h')
   .alias('h', 'help')
   .argv;
